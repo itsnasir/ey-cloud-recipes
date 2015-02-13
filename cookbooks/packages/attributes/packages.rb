423536b697cab07393659e364e7ef943fd5e1a87
nasirvq <itsnasir@gmail.com>
2015-02-13 18:55:20 +0500

    removed extra space

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -7,4 +7,4 @@
 # Examples below:
 
 default[:packages] = [{:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.1"},
-                       {:name => " dev-db/freetds", :version => "0.91"}]
+                       {:name => "dev-db/freetds", :version => "0.91"}]
